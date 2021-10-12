b557a3ee3b0cd6724ed399afc01e6eb502f6ad68
Max Rusakovič <maxrusakovic@gmail.com>
2021-10-12 20:55:32 +0200

    [1] Initialized React Native

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const App = () => {
+  return <SafeAreaView style={{flex: 1}}></SafeAreaView>;
+};
+
+const styles = StyleSheet.create();
+
+export default App;
---
+++ babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.jsx',
+          '.js',
+          '.tsx',
+          '.ts',
+          '.json',
+        ],
+        alias: {
+          '@components': './components',
+          '@assets': './assets',
+        },
+      },
+    ],
+  ],
+};
